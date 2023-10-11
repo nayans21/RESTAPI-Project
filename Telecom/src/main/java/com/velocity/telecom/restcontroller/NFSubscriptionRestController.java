6ace3dd12cb3270f34618984e49b1c3a464338e1
nayans21 <sawantnayan5@gmail.com>
2023-10-11 14:44:38 +0530

    Added updated code to update the NetworkSliceCondition

--- Telecom/src/main/java/com/velocity/telecom/restcontroller/NFSubscriptionRestController.java
+++ Telecom/src/main/java/com/velocity/telecom/restcontroller/NFSubscriptionRestController.java
@@ -1,24 +1,34 @@
 package com.velocity.telecom.restcontroller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.velocity.telecom.model.NFSubscriptionData;
+import com.velocity.telecom.model.NetworkSliceCondition;
 import com.velocity.telecom.service.NFSubscriptionDataService;
+import com.velocity.telecom.service.NetworkSliceConditionService;
 
 @RestController
 @RequestMapping("/nfSubscription")
 public class NFSubscriptionRestController {
 
 	private NFSubscriptionDataService nfSubscriptionDataService;
+	private NetworkSliceConditionService networkSliceConditionService;
 	
 	@PutMapping("/update")
 	public NFSubscriptionData updateNFSubscription(@RequestBody NFSubscriptionData nfSubscriptionData) {
 		
-		return nfSubscriptionDataService.updateNFSubscriptionData(nfSubscriptionData);
-		
+		NFSubscriptionData nfSub = nfSubscriptionDataService.updateNFSubscriptionData(nfSubscriptionData);
+		List <NetworkSliceCondition> networkSliceList = nfSubscriptionData.getNetworkSliceConditions();
+		for(NetworkSliceCondition networkSlice : networkSliceList) {
+			networkSlice.setNfstatusNotificationUriId(nfSubscriptionData.getNfstatusNotificationUri());
+			networkSliceConditionService.updateNetworkSliceCondition(networkSlice);
+		}
+		return nfSub;
 	}
 }
 
