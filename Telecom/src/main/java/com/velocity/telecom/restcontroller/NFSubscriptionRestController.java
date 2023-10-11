a5fa5ac6bcf5d46f0e62824c5df7a4d325c25c01
nayans21 <sawantnayan5@gmail.com>
2023-10-11 14:25:03 +0530

    Updated the Put request code.

--- Telecom/src/main/java/com/velocity/telecom/restcontroller/NFSubscriptionRestController.java
+++ Telecom/src/main/java/com/velocity/telecom/restcontroller/NFSubscriptionRestController.java
@@ -1,5 +1,24 @@
 package com.velocity.telecom.restcontroller;
 
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.velocity.telecom.model.NFSubscriptionData;
+import com.velocity.telecom.service.NFSubscriptionDataService;
+
+@RestController
+@RequestMapping("/nfSubscription")
 public class NFSubscriptionRestController {
 
+	private NFSubscriptionDataService nfSubscriptionDataService;
+	
+	@PutMapping("/update")
+	public NFSubscriptionData updateNFSubscription(@RequestBody NFSubscriptionData nfSubscriptionData) {
+		
+		return nfSubscriptionDataService.updateNFSubscriptionData(nfSubscriptionData);
+		
+	}
 }
+
