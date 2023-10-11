e67f926285a97b9d2a4fe803d86981f59f918324
nayans21 <sawantnayan5@gmail.com>
2023-10-11 17:41:55 +0530

    Added new Put Requests

--- Telecom/src/main/java/com/velocity/telecom/restcontroller/NFSubscriptionRestController.java
+++ Telecom/src/main/java/com/velocity/telecom/restcontroller/NFSubscriptionRestController.java
@@ -3,10 +3,15 @@ package com.velocity.telecom.restcontroller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.velocity.telecom.model.NFSubscriptionData;
 import com.velocity.telecom.model.NetworkSliceCondition;
@@ -29,9 +34,28 @@ public class NFSubscriptionRestController {
 		List <NetworkSliceCondition> networkSliceList = nfSubscriptionData.getNetworkSliceConditions();
 		for(NetworkSliceCondition networkSlice : networkSliceList) {
 			
-			networkSliceConditionService.updateNetworkSliceCondition(networkSlice);
-		}
+				networkSliceConditionService.updateNetworkSliceCondition(networkSlice);
+			}
 		return nfSub;
+	}
+	
+	@PutMapping("/update/networkslicecondition")
+	public NetworkSliceCondition updateNetworkSliceConditionById(@RequestBody NetworkSliceCondition networkSlice) {
+
+		return networkSliceConditionService.updateNetworkSliceCondition(networkSlice);
+		
+	}
+	
+	@PutMapping("/update/nfSubscriptionData")
+	public NFSubscriptionData updateNFSubscriptionData(@RequestBody NFSubscriptionData nfSubscriptionData) {
+
+		if(nfSubscriptionData.getNetworkSliceConditions()==null) {
+			
+			return nfSubscriptionDataService.updateNFSubscriptionData(nfSubscriptionData);
+		}
+		else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Network Slice Condition data is not required.");
+		}		
 	}
 }
 
