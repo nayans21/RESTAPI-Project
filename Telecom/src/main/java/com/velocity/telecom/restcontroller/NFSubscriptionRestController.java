85ef252daac691963f847ff1a5af28ba06e7286d
HP <HP@DESKTOP-728R74P>
2023-10-11 13:50:05 +0530

    request added to push

--- Telecom/src/main/java/com/velocity/telecom/restcontroller/NFSubscriptionRestController.java
+++ Telecom/src/main/java/com/velocity/telecom/restcontroller/NFSubscriptionRestController.java
@@ -2,6 +2,8 @@ package com.velocity.telecom.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Description;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class NFSubscriptionRestController<NFsubscription> {
 
 	@Autowired
 	private NFsubscription  nfSubscription; 
-	
+	@PutMapping("/updatenfSubscription")
+	public Description
+	saveNfsubscription(@RequestBody NfSubscription nfsubscription )	{
+		nfsubscription nfsub=nfsubscriptionService.updateNfsubscription(nfsubscription);
+		return nfsub;
+	}
 
-}
+
