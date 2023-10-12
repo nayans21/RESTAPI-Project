b51e6cf54517d14f65b82b1d715a791cca67d043
nayans21 <sawantnayan5@gmail.com>
2023-10-12 13:50:03 +0530

    Removed unused imports

--- Telecom/src/main/java/com/velocity/telecom/restcontroller/NFSubscriptionRestController.java
+++ Telecom/src/main/java/com/velocity/telecom/restcontroller/NFSubscriptionRestController.java
@@ -3,17 +3,12 @@ package com.velocity.telecom.restcontroller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.velocity.telecom.exception.CanNotPerformOperation;
 import com.velocity.telecom.model.NFSubscriptionData;
 import com.velocity.telecom.model.NetworkSliceCondition;
